77952436551e3c8fc4c066abacdc83141e8bf239
1Donat1 <79876158+1Donat1@users.noreply.github.com>
2022-03-26 22:55:25 +0200

    Initial commit

---
+++ L3/task1/script/main.js
@@ -0,0 +1,11 @@
+'use srict'
+num1= Boolean(Number('10')) + 1; //  boolean=true. true=1. 1+1=2
+num2= 'sub'  + Number(false);//false=0.sub+0=sub0
+num3=16  *  '    91  '   ;//1456
+num4=true-70;//true=1. 1-70=-69
+num5=Number(1 + String(1)) + 1; //11+1=12
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(num5);
